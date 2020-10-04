1f84e931dd294905fb67a6ca24de3d63ba72cf3d
“Mrityunjay <mrityunjay38@gmail.com>
2020-10-04 21:28:48 +0530

    Add Canvas component

---
+++ src/components/Canvas/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const Canvas = () => {
+    return (
+        <div className="canvas">
+            <canvas className="whiteboard" />
+        </div>
+    )
+}
+
+export default Canvas;
--- src/components/PenPad/index.jsx
+++ src/components/PenPad/index.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import Row from '../Layouts/Row';
-import Col from '../Layouts/Col';
-import Tools from '../Tools';
-import './index.css';
+import React from "react";
+import Row from "../Layouts/Row";
+import Col from "../Layouts/Col";
+import Tools from "../Tools";
+import "./index.css";
+import Whiteboard from "../Canvas";
 
 const PenPad = () => {
-    return (
-        <div className="pen-pad">
-            <Col width="5em">
-                <Tools />
-            </Col>
-            <Col></Col>
-        </div>
-    )
-}
+  return (
+    <div className="pen-pad">
+      <Col width="5em">
+        <Tools />
+      </Col>
+      <Col>
+        <Whiteboard />
+      </Col>
+    </div>
+  );
+};
 
 export default PenPad;
