c8fb80bc65549ba362c902778f1e6458eae66101
“Mrityunjay <mrityunjay38@gmail.com>
2020-10-18 00:09:29 +0530

    Setup drawable canvas

--- src/components/PenPad/index.jsx
+++ src/components/PenPad/index.jsx
@@ -9,6 +9,7 @@ import { useSelector } from "react-redux";
 const PenPad = () => {
   const boards = useSelector((state) => state.boards);
   const view = useSelector(state => state.view.viewType);
+  const {width, height} = useConstructCanvas();
 
   return (
     <Row className="pen-pad">
@@ -17,10 +18,10 @@ const PenPad = () => {
       </Col>
       <Col>
         <Row className={view}>
-          <Col className="main-content">
+          <Col className="main-content" width={width} height={height}>
           <Row className="boards">
             {boards.map((board, id) => (
-              <Whiteboard key={id} />
+              <Whiteboard key={id} width={width} height={height} />
             ))}
           </Row>
           <StatusBar />
@@ -30,5 +31,23 @@ const PenPad = () => {
     </Row>
   );
 };
+
+const useConstructCanvas = () => {
+  const screenWidth = window.screen.width;
+  const screenHeight = window.screen.height;
+
+    if(screenWidth > screenHeight){
+      return {
+        width: screenWidth - screenWidth * 0.10, // reduce canvas width by 15%
+        height: screenHeight - screenHeight * 0.15 // reduce canvas height by 10%
+      }
+    }
+    else {
+      return {
+        width: screenWidth - screenWidth * 0.15, // reduce canvas width by 10%
+        height: screenHeight - screenHeight * 0.10 // reduce canvas height by 10%
+      }
+    }
+}
 
 export default PenPad;
