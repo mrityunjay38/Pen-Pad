6158d540daa8560f50c0c038189820883ee328e4
Mrityunjay <mrityun.jay@indegene.com>
2020-10-25 00:55:12 +0530

    Canvas view mode change (grid)

--- src/components/PenPad/index.jsx
+++ src/components/PenPad/index.jsx
@@ -12,7 +12,7 @@ const { LANDSCAPE_VIEW, PORTRAIT_VIEW, GRID_VIEW } = actionTypes;
 const PenPad = () => {
   const boards = useSelector((state) => state.boards);
   const view = useSelector(state => state.view.viewType);
-  const {width, height} = useConstructCanvas();
+  const {width, height, viewWidth, viewHeight} = useConstructCanvas();
 
   return (
     <Row className="pen-pad">
@@ -21,7 +21,7 @@ const PenPad = () => {
       </Col>
       <Col>
         <Row className={view}>
-          <Col className="main-content" width={width} height={height}>
+          <Col className="main-content" width={viewWidth} height={viewHeight}>
           <Row className="boards">
             {boards.map((board, id) => (
               <Whiteboard key={id} width={width} height={height - 32} /> // subtract statusbar height
@@ -36,22 +36,29 @@ const PenPad = () => {
 };
 
 const useConstructCanvas = () => {
-  const [canvasSize,setCanvasSize] = useState({width: 0, height: 0});
+  const [canvasSize,setCanvasSize] = useState({width: 0, height: 0,viewWidth: 0,viewHeight: 0});
   const view = useSelector(state => state.view.viewType);
   const screenWidth = window.screen.width;
   const screenHeight = window.screen.height;
 
   useEffect(() => {
-    let [width,height] = [null,null];
+    let [width,height,viewWidth,viewHeight] = [null,null];
     if(view === LANDSCAPE_VIEW){
         width = screenWidth - screenWidth * 0.10; // reduce canvas width by 10%
         height = screenHeight - screenHeight * 0.15; // reduce canvas height by 15%
-        setCanvasSize({width,height});
+        setCanvasSize({width,height,viewWidth: width,viewHeight: height});
     }
     else if(view === PORTRAIT_VIEW) {
         width = screenWidth - screenWidth * 0.5; // reduce canvas width by 50%
         height = screenHeight - screenHeight * 0.15; // reduce canvas height by 15%
-        setCanvasSize({width,height});
+        setCanvasSize({width,height,viewWidth: width,viewHeight:height});
+    }
+    else if(view === GRID_VIEW) {
+      width = Math.floor((screenWidth - screenWidth * 0.10)/2); // reduce canvas width by 10%, half it and round off to lower val 
+      height = screenHeight - screenHeight * 0.15; // reduce canvas height by 15%
+      viewWidth = screenWidth - screenWidth * 0.10; // reduce board width by 10%
+      viewHeight = screenHeight - screenHeight * 0.15; // reduce board height by 15%
+      setCanvasSize({width,height,viewWidth,viewHeight});
     }
   },[view,screenWidth,screenHeight]);
   return canvasSize;
