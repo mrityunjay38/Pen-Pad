eda7c99dff8ca015df1c74f7cc53fa70e43f7595
Mrityunjay <mrityun.jay@indegene.com>
2020-10-26 19:18:37 +0530

    Updates

---
+++ precache-manifest.3508f84bee43658fa1c875211e3c4736.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd824aa0d8b35caf9570352b3a18179",
+    "url": "/Pen-Pad/index.html"
+  },
+  {
+    "revision": "95666ac2c29b7757230c",
+    "url": "/Pen-Pad/static/css/main.48e87a51.chunk.css"
+  },
+  {
+    "revision": "cc4ed9f9b77bedeb4a51",
+    "url": "/Pen-Pad/static/js/2.9d7ce6c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Pen-Pad/static/js/2.9d7ce6c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95666ac2c29b7757230c",
+    "url": "/Pen-Pad/static/js/main.17b08525.chunk.js"
+  },
+  {
+    "revision": "8b711511463f8f1e5f28",
+    "url": "/Pen-Pad/static/js/runtime-main.b0d9672a.js"
+  },
+  {
+    "revision": "9d8f0f412d3d013a6ff10957eb8bbe60",
+    "url": "/Pen-Pad/static/media/add.9d8f0f41.svg"
+  },
+  {
+    "revision": "651f3c8159471e8d2f28db013e44d947",
+    "url": "/Pen-Pad/static/media/download-all.651f3c81.svg"
+  },
+  {
+    "revision": "44177b51ba009410fbca766615e8e471",
+    "url": "/Pen-Pad/static/media/download.44177b51.svg"
+  },
+  {
+    "revision": "87cd4b9be50352e60d7b6f6653049a5d",
+    "url": "/Pen-Pad/static/media/eraser.87cd4b9b.svg"
+  },
+  {
+    "revision": "819ff0b63667d9e9633ecd4578a41c17",
+    "url": "/Pen-Pad/static/media/grid.819ff0b6.svg"
+  },
+  {
+    "revision": "336ec3e4195892f946430c3163d3c1d8",
+    "url": "/Pen-Pad/static/media/landscape-view.336ec3e4.svg"
+  },
+  {
+    "revision": "349d5154da9463f7144e74f6b6cf7cb8",
+    "url": "/Pen-Pad/static/media/minus.349d5154.svg"
+  },
+  {
+    "revision": "600d57a3094c557e70009b13308c2871",
+    "url": "/Pen-Pad/static/media/palette.600d57a3.svg"
+  },
+  {
+    "revision": "f4f2b90bfd58e36fd6fce237c6a70f35",
+    "url": "/Pen-Pad/static/media/pen.f4f2b90b.svg"
+  },
+  {
+    "revision": "6e9482d1c52498ded07f880e7d052bc3",
+    "url": "/Pen-Pad/static/media/plus.6e9482d1.svg"
+  },
+  {
+    "revision": "eafdfff99ce8f499e8785d36304e7c41",
+    "url": "/Pen-Pad/static/media/portrait-view.eafdfff9.svg"
+  },
+  {
+    "revision": "c3c241acd9a896a3d622348d06c2b6ab",
+    "url": "/Pen-Pad/static/media/trash.c3c241ac.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Pen-Pad/precache-manifest.3508f84bee43658fa1c875211e3c4736.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Pen-Pad/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
